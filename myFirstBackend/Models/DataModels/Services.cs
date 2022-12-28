8201a8bf8c862bde8df13f20c0c891eeb24c4723
Erick Penado <penado188@gmail.com>
2022-12-27 20:45:03 -0600

    Create Services class that contains methods for the requested searches

---
+++ myFirstBackend/Models/DataModels/Services.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace myFirstBackend.Models.DataModels
+{
+    public class Services
+    {
+
+        public static IEnumerable<User> SearchForEmail(string Email)
+        {
+            var newUser = new[] {
+                new User()
+            };
+
+            var UserForEmail = newUser.Where(user => user.Email == Email);
+
+            return UserForEmail;
+        }
+    }
+}
